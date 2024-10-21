8d4174c657a4e0731ce36cf6060243a6a79ec610
aristokratoss <oluwatobilobao@examroom.ai>
2024-10-21 18:02:39 +0100

    Resume display MVC dotnet project

---
+++ ResumeDisplayProject/Controllers/CVController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResumeDisplayProject.Models;
+
+namespace ResumeDisplayProject.Controllers
+{
+    public class CvController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Experience()
+        {
+            return View();
+        }
+
+        public ActionResult Skills()
+        {
+            return View();
+        }
+
+        // GET: /Cv/TaxCalculator
+        [HttpGet]
+        public ActionResult TaxCalculator()
+        {
+            // Return the view for the tax calculator
+            var model = new TaxViewModel();
+            return View(model);
+        }
+
+        // POST: /Cv/CalculateTax
+        [HttpPost]
+        public ActionResult CalculateTax(TaxViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Income > 0)
+                {
+                    model.Tax = CalculateTaxBasedOnIncome(model.Income);
+                }
+            }
+            return View("TaxCalculator", model);
+        }
+
+        private decimal CalculateTaxBasedOnIncome(decimal income)
+        {
+            if (income <= 50000)
+                return income * 0.1m;
+            else if (income <= 100000)
+                return income * 0.2m;
+            else
+                return income * 0.3m;
+        }
+
+
+    }
+
+}
---
+++ ResumeDisplayProject/Models/TaxViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ResumeDisplayProject.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class TaxViewModel
+    {
+        [Required(ErrorMessage = "Please enter your income.")]
+        [Range(0, double.MaxValue, ErrorMessage = "Income must be a positive number.")]
+        public decimal Income { get; set; }
+
+        public decimal? Tax { get; set; }
+    }
+
+}
